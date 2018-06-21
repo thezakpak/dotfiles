e9429d85ab2e59b07d464f201296f7e123312ced
bmf <bmf.infomation@gmail.com>
2018-06-21 23:13:32 +0900

    [update]

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 ln -sf ${PWD}/.vimrc ~/.vimrc
 ln -sf ${PWD}/.tmux.conf ~/.tmux.conf
-ln -sf ${PWD}/keymap.cson ~/.atom/keymap.cson
 ln -sf ${PWD}/.inputrc ~/.inputrc
 ln -sf ${PWD}/.bashrc ~/.bashrc
+ln -sf ${PWD}/.atom/conig.cson ~/.atom/config.cson
+ln -sf ${PWD}/.atom/init.coffee ~/.atom/init.coffee
+ln -sf ${PWD}/.atom/keymap.cson ~/.atom/keymap.cson
+ln -sf ${PWD}/.atom/snippets.cson ~/.atom/snippets.cson
+ln -sf ${PWD}/.atom/styles.less ~/.atom/styles.less
