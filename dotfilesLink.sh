a929a25f58900a2eb3d696e010b7c222e43fdf1f
bmf <bmf.infomation@gmail.com>
2018-06-22 10:58:21 +0900

    [update]

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -8,3 +8,9 @@ ln -sf ${PWD}/.atom/init.coffee ~/.atom/init.coffee
 ln -sf ${PWD}/.atom/keymap.cson ~/.atom/keymap.cson
 ln -sf ${PWD}/.atom/snippets.cson ~/.atom/snippets.cson
 ln -sf ${PWD}/.atom/styles.less ~/.atom/styles.less
+
+# ATOM
+apm install --packages-file ${PWD}/.atom/package.list
+# If you want to update package.list, run this command.
+# apm list --installed --bare packages.list
+
