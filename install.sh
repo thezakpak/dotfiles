0b52b168c2e9d744f59d8fefc38e63c9c77f778f
bmf <bmf.infomation@gmail.com>
2018-10-08 13:19:13 +0900

    [update]

--- install.sh
+++ install.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 ln -sf ${PWD}/.vimrc ${HOME}/.vimrc
 ln -sf ${PWD}/.tmux.conf ${HOME}/.tmux.conf
-l -sf ${PWD}/.inputrc ${HOME}/.inputrc
+ln -sf ${PWD}/.inputrc ${HOME}/.inputrc
 ln -sf ${PWD}/.bashrc ${HOME}/.bashrc
 ln -sf ${PWD}/.atom/conig.cson ${HOME}/.atom/config.cson
 ln -sf ${PWD}/.atom/init.coffee ${HOME}/.atom/init.coffee
 ln -sf ${PWD}/.atom/keymap.cson ${HOME}/.atom/keymap.cson
 ln -sf ${PWD}/.atom/snippets.cson ${HOME}/.atom/snippets.cson
 ln -sf ${PWD}/.atom/styles.less ${HOME}/.atom/styles.less
+ln -sh ${PWD}/iterm2/com.googlecode.iterm2.plish ${HOME}/Library/Preferences/com.googlecode.iterm2.plist
 
 # atom
 apm install --packages-file ${PWD}/.atom/package.list
